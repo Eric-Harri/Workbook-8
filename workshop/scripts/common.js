ec3c2adf408ef39b90ce1c20a7c5f48223839f14
Eric Harris <ericoldie@gmail.com>
2022-11-28 09:21:11 -0500

    finished functionality

--- workshop/scripts/common.js
+++ workshop/scripts/common.js
@@ -1,1 +1,40 @@
 const $q = (s) => document.querySelector(s);
+
+function loadUsers() { 
+    return fetch("http://localhost:8083/api/users/").then((response) =>
+        response.json()
+    );
+}
+
+function getUserId() {
+    const params = new URLSearchParams(window.location.search);
+    if (!params.has("userId")) return undefined;
+    return Number(params.get("userId"));
+}
+
+function fillUsersDropDown(users, selectedUserId = 0) {
+    for (const user of users) {
+        const option = document.createElement("option");
+        option.value = user.id;
+        option.innerText = user.name;
+        if (user.id === selectedUserId) {
+            option.selected = true;
+        }
+
+        usersDropDownList.appendChild(option);
+    }
+}
+
+function loadCategories() {
+    return fetch("http://localhost:8083/api/categories/")
+        .then((response) => response.json());
+}
+
+function fillCategoriesDropDown(categories) {
+    for (const category of categories) {
+        const option = document.createElement("option");
+        option.value = category.id;
+        option.innerText = category.name;
+        categoriesDropdownList.appendChild(option)
+    }
+}
